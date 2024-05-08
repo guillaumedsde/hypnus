9af491be70434da2b1c07ca4cc0cd6c3616b1642
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2024-05-08 16:37:16 +0200

    feat: shutdown exception

--- morpheus/power.py
+++ morpheus/power.py
@@ -5,16 +5,24 @@ import platform
 class ShutdownNotImplementedForPlatformError(NotImplementedError): ...
 
 
+class CouldNotShutdownError(RuntimeError): ...
+
+
 def shutdown() -> None:
     """Shutdown the computer, supports multiple platforms.
 
     Raises:
-        ShutdownNotImplementedForPlatformError: When current platform not supported.
+        ShutdownNotImplementedForPlatformError:
+            When current platform not supported.
+        CouldNotShutdownError:
+            When an error occured while shutting down computer.
     """
     match platform.system():
         case "Windows":
-            os.system("shutdown -s")
+            exit_code = os.system("shutdown -s")
         case "Linux" | "Darwin":
-            os.system("shutdown -h now")
+            exit_code = os.system("shutdown -h now")
         case _:
             raise ShutdownNotImplementedForPlatformError
+    if exit_code != 0:
+        raise CouldNotShutdownError
