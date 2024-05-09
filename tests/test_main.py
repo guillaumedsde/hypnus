f94828d25d9ec4bef7cac96440a1ea66a4d9e1d5
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2024-05-09 19:44:02 +0200

    test: main

---
+++ tests/test_main.py
@@ -0,0 +1,30 @@
+from unittest import mock
+
+import pytest
+
+from morpheus import main
+from morpheus.tempo import TempoDayColor
+
+
+@pytest.mark.parametrize(
+    "tempo_color",
+    TempoDayColor,
+    ids=TempoDayColor._member_names_,
+)
+@mock.patch.object(main.power, "shutdown", autospec=True)
+@mock.patch.object(main.tempo, "get_todays_tempo_color", autospec=True)
+def test_main_shutdown_on_day_color(
+    mock_get_todays_tempo_color: mock.MagicMock,
+    mock_shutdown: mock.MagicMock,
+    tempo_color: TempoDayColor,
+) -> None:
+    mock_get_todays_tempo_color.return_value = tempo_color
+
+    main._main()
+
+    mock_get_todays_tempo_color.assert_called_once_with()
+
+    if tempo_color is TempoDayColor.RED:
+        mock_shutdown.assert_called_once_with()
+    else:
+        mock_shutdown.assert_not_called()
