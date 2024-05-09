d792fcc797c86661d85cb468942ebc1404b31d74
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2024-05-09 20:28:28 +0200

    chore: type ignore

--- morpheus/power.py
+++ morpheus/power.py
@@ -16,7 +16,8 @@ SHTDN_REASON_MAJOR_OTHER = 0x00000000
 
 
 def _windows_shutdown() -> None:
-    user32 = ctypes.WinDLL("user32")
+    # NOTE: we run mypy on linux so it thinks ctypes.WinDLL does not exist
+    user32 = ctypes.WinDLL("user32")  # type: ignore[attr-defined]
     user32.ExitWindowsEx(EWX_POWEROFF, SHTDN_REASON_MAJOR_OTHER)
 
 
